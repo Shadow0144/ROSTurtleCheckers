9c9aeea6c22312170e692548c61aec8fb6890934
Shadow0144 <corbincogswell@gmail.com>
2025-08-06 08:23:56 -0500

    Better layout for the main menu

--- src/player/frame/MainMenuFrame.cpp
+++ src/player/frame/MainMenuFrame.cpp
@@ -29,49 +29,57 @@ MainMenuFrame::MainMenuFrame(
     auto titleWidget = new TitleWidget();
     mainLayout->addWidget(titleWidget);
 
-    auto playerNameLayout = new QHBoxLayout();
-
     m_playerNameLabel = new QLabel("");
     auto playerNameFont = m_playerNameLabel->font();
     playerNameFont.setPointSize(PLAYER_NAME_FONT_SIZE);
     m_playerNameLabel->setFont(playerNameFont);
-    playerNameLayout->addWidget(m_playerNameLabel);
+    m_playerNameLabel->setAlignment(Qt::AlignCenter);
+    m_playerNameLabel->setContentsMargins(0, 0, 0, 20);
+    mainLayout->addWidget(m_playerNameLabel);
 
-    std::string statisticsString = "Statistics";
-    auto statisticsButton = new QPushButton(statisticsString.c_str());
-    statisticsButton->setFixedWidth(MENU_BUTTON_WIDTH);
-    connect(statisticsButton, &QPushButton::released, this, &MainMenuFrame::handleStatisticsButton);
-    playerNameLayout->addWidget(statisticsButton);
-
-    std::string logOutAccountString = "Log Out";
-    auto logOutAccountButton = new QPushButton(logOutAccountString.c_str());
-    logOutAccountButton->setFixedWidth(MENU_BUTTON_WIDTH);
-    connect(logOutAccountButton, &QPushButton::released, this, &MainMenuFrame::handleLogOutAccountButton);
-    playerNameLayout->addWidget(logOutAccountButton);
-
-    mainLayout->addLayout(playerNameLayout);
-
-    auto buttonLayout = new QHBoxLayout();
-
+    auto createLobbyLayout = new QHBoxLayout();
     std::string createLobbyString = "Create Lobby";
     m_createLobbyButton = new QPushButton(createLobbyString.c_str());
     m_createLobbyButton->setFixedWidth(MENU_BUTTON_WIDTH);
     connect(m_createLobbyButton, &QPushButton::released, this, &MainMenuFrame::handleCreateLobbyButton);
-    buttonLayout->addWidget(m_createLobbyButton);
+    createLobbyLayout->addWidget(m_createLobbyButton);
+    mainLayout->addLayout(createLobbyLayout);
 
+    auto joinLobbyLayout = new QHBoxLayout();
     std::string joinLobbyString = "Join Lobby";
     m_joinLobbyButton = new QPushButton(joinLobbyString.c_str());
     m_joinLobbyButton->setFixedWidth(MENU_BUTTON_WIDTH);
+    m_joinLobbyButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
     connect(m_joinLobbyButton, &QPushButton::released, this, &MainMenuFrame::handleJoinLobbyButton);
-    buttonLayout->addWidget(m_joinLobbyButton);
+    joinLobbyLayout->addWidget(m_joinLobbyButton);
+    mainLayout->addLayout(joinLobbyLayout);
 
+    auto statisticsLayout = new QHBoxLayout();
+    std::string statisticsString = "Statistics";
+    auto statisticsButton = new QPushButton(statisticsString.c_str());
+    statisticsButton->setFixedWidth(MENU_BUTTON_WIDTH);
+    statisticsButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
+    connect(statisticsButton, &QPushButton::released, this, &MainMenuFrame::handleStatisticsButton);
+    statisticsLayout->addWidget(statisticsButton);
+    mainLayout->addLayout(statisticsLayout);
+
+    auto logOutLayout = new QHBoxLayout();
+    std::string logOutAccountString = "Log Out";
+    auto logOutAccountButton = new QPushButton(logOutAccountString.c_str());
+    logOutAccountButton->setFixedWidth(MENU_BUTTON_WIDTH);
+    logOutAccountButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
+    connect(logOutAccountButton, &QPushButton::released, this, &MainMenuFrame::handleLogOutAccountButton);
+    logOutLayout->addWidget(logOutAccountButton);
+    mainLayout->addLayout(logOutLayout);
+
+    auto quitLayout = new QHBoxLayout();
     std::string quitString = "Quit";
     auto quitButton = new QPushButton(quitString.c_str());
     quitButton->setFixedWidth(MENU_BUTTON_WIDTH);
+    quitButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Preferred);
     connect(quitButton, &QPushButton::released, this, &MainMenuFrame::handleQuitButton);
-    buttonLayout->addWidget(quitButton);
-
-    mainLayout->addLayout(buttonLayout);
+    quitLayout->addWidget(quitButton);
+    mainLayout->addLayout(quitLayout);
 }
 
 MainMenuFrame::~MainMenuFrame()
